e2b17f2ff46324885ed09d07e64d8733b6acc4df
Arina <hadson.rey@mail.ru>
2020-05-08 19:06:00 +0300

    создает отдельные компоненты для добавления эмоции, и компонент текстовое поле

---
+++ src/components/adding-emoji.js
@@ -0,0 +1,29 @@
+import AbstractSmartComponent from "./abstract-smart";
+
+const createAddingEmojiTemplate = (currentEmoji) => {
+  return (
+    `<div for="add-emoji" class="film-details__add-emoji-label">
+       ${currentEmoji ? `<img src="./images/emoji/${currentEmoji}.png" width="55" height="55" alt="emoji">` : ``}
+    </div>`
+  );
+};
+
+export default class AddingEmoji extends AbstractSmartComponent {
+  constructor(currentEmoji) {
+    super();
+    this._currentEmoji = currentEmoji;
+  }
+
+  getTemplate() {
+    return createAddingEmojiTemplate(this._currentEmoji);
+  }
+
+  recoveryListeners() {
+    return null;
+  }
+
+  rerender(emoji) {
+    this._currentEmoji = emoji;
+    super.rerender();
+  }
+}
