6215371027f56ad3bf0844dafc72c41db33e87c9
Arina <hadson.rey@mail.ru>
2020-05-14 19:14:37 +0300

    добавляет реализацию перерисовки компонента сортировки, учитывая активный.

--- src/components/sort-elements.js
+++ src/components/sort-elements.js
@@ -1,28 +1,52 @@
-import AbstractComponent from "./abstract";
+import AbstractSmartComponent from "./abstract-smart";
 import {SortTypes} from "../utils/sorting";
+import {ACTIVE_CLASS_SORT} from "../utils/utils";
 
-const createSortElementsTemplate = () => {
+const createSortItem = (element) => {
+  const {
+    name,
+    isActive,
+  } = element;
+
+  return (
+    `<li>
+      <a href="#" data-sort-type="${name}" class="sort__button ${isActive ? ACTIVE_CLASS_SORT : ``}">Sort by ${name}</a>
+    </li>`
+  );
+};
+
+const createSortElementsTemplate = (elements) => {
+  const sortItems = elements
+    .map((element) => createSortItem(element))
+    .join(`\n`);
+
   return (
     `<ul class="sort">
-      <li><a href="#" data-sort-type="${SortTypes.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" data-sort-type="${SortTypes.DATE}" class="sort__button">Sort by date</a></li>
-      <li><a href="#" data-sort-type="${SortTypes.RATING}" class="sort__button">Sort by rating</a></li>
+      ${sortItems}
     </ul>`
   );
 };
 
-export default class SortElement extends AbstractComponent {
-  constructor() {
+export default class SortElement extends AbstractSmartComponent {
+  constructor(sortElements) {
     super();
 
+    this._sortElements = sortElements;
     this._currentSortType = SortTypes.DEFAULT;
   }
 
   getTemplate() {
-    return createSortElementsTemplate();
+    return createSortElementsTemplate(this._sortElements);
+  }
+
+  setSortType(sortElements) {
+    this._sortElements = sortElements;
+    this.rerender();
   }
 
   setSortTypeChangeHandler(handler) {
+    this._handler = handler;
+
     this.getElement()
       .addEventListener(`click`, (evt) => {
         evt.preventDefault();
@@ -41,5 +65,9 @@ export default class SortElement extends AbstractComponent {
 
         handler(this._currentSortType);
       });
+  }
+
+  recoveryListeners() {
+    this.setSortTypeChangeHandler(this._handler);
   }
 }
