e2ff600e649c0912c587309d654ffc6e42f6bcf1
Arina <hadson.rey@mail.ru>
2020-05-26 12:36:54 +0300

    рефакторинг компонентов (количество фильмов и грейд юзера)

--- src/components/user-profile.js
+++ src/components/user-profile.js
@@ -1,22 +1,25 @@
-import AbstractComponent from "./abstract";
+import AbstractSmartComponent from "./abstract-smart";
 
 const createUserProfileTemplate = (grade) => {
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">${grade}</p>
+      ${grade ? `<p class="profile__rating">${grade}</p>` : ``}
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`
   );
 };
 
-export default class UserProfile extends AbstractComponent {
-  constructor(grade) {
-    super();
-
+export default class UserProfile extends AbstractSmartComponent {
+  setGrade(grade) {
     this._grade = grade;
+    this.rerender();
   }
 
   getTemplate() {
     return createUserProfileTemplate(this._grade);
+  }
+
+  recoveryListeners() {
+    return null;
   }
 }
